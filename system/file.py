77938d68be5ff2c17932674640b9f68b57c225ee
Paris Nakita Kejser <paris@fcomputer.dk>
2015-05-12 14:11:26 +0200

    (bugfix) error - removed file
    this file have a error, its never remove a single file, its now fixed.

--- system/file.py
+++ system/file.py
@@ -3,18 +3,18 @@ from stat import *
 
 class File:
     debug = "on"
-
+    
     """
-    Debug messegt will be printet to terminal if debugLevel is on
-    """
+        Debug messegt will be printet to terminal if debugLevel is on
+        """
     def __debug(self,msg):
         if ( self.debug.lower() == "on" ):
             print msg
 
-    """
+"""
     Remove all files insite of this current folder.
     """
-    def remove_all_files_in_folder(self,folder_name):
+        def remove_all_files_in_folder(self,folder_name):
         
         for file in os.listdir( folder_name ):
             pathname = os.path.join(folder_name, file)
@@ -25,14 +25,13 @@ class File:
                 self.__debug("Skip this is a folder: %s" % file )
             elif S_ISREG(mode):
                 try:
-                    os.remove(folder_name + file )
-                    self.__debug("Remove file: %s" % folder_name + file )
+                    self.__debug("Remove file: %s" % folder_name +"/"+ file )
+                    os.remove(folder_name +"/"+ file )
+                
                 except OSError:
+                    self.__debug("Error: file not removed." )
                     pass
-            else:
-                self.__debug("Its not a file or a folder - skipping: %s" % file )
-
+else:
+    self.__debug("Its not a file or a folder - skipping: %s" % file )
+        
         self.__debug("Alle files in {%s} folder are removed" % folder_name)
-
-
-    
